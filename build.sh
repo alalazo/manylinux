df33ee23e02ffa5c43fc13c1fe0088d23e9b6b84
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-04-09 10:13:17 +0200

    Modified build workflow and scripts to maintain libpython.a

--- build.sh
+++ build.sh
@@ -8,56 +8,20 @@ export POLICY
 export PLATFORM
 
 # get docker default multiarch image prefix for PLATFORM
-if [ "${PLATFORM}" == "x86_64" ]; then
-	MULTIARCH_PREFIX="amd64/"
-elif [ "${PLATFORM}" == "i686" ]; then
-	MULTIARCH_PREFIX="i386/"
-elif [ "${PLATFORM}" == "aarch64" ]; then
+if [ "${PLATFORM}" == "aarch64" ]; then
 	MULTIARCH_PREFIX="arm64v8/"
 elif [ "${PLATFORM}" == "ppc64le" ]; then
 	MULTIARCH_PREFIX="ppc64le/"
-elif [ "${PLATFORM}" == "s390x" ]; then
-	MULTIARCH_PREFIX="s390x/"
 else
 	echo "Unsupported platform: '${PLATFORM}'"
 	exit 1
 fi
 
-# setup BASEIMAGE and its specific properties
-if [ "${POLICY}" == "manylinux2010" ]; then
-	if [ "${PLATFORM}" == "x86_64" ]; then
-		BASEIMAGE="quay.io/pypa/manylinux2010_x86_64_centos6_no_vsyscall"
-	elif [ "${PLATFORM}" == "i686" ]; then
-		BASEIMAGE="${MULTIARCH_PREFIX}centos:6"
-	else
-		echo "Policy '${POLICY}' does not support platform '${PLATFORM}'"
-		exit 1
-	fi
-	DEVTOOLSET_ROOTPATH="/opt/rh/devtoolset-8/root"
-	PREPEND_PATH="${DEVTOOLSET_ROOTPATH}/usr/bin:"
-	if [ "${PLATFORM}" == "i686" ]; then
-		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst"
-	else
-		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib64:${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib64/dyninst:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst:/usr/local/lib64"
-	fi
-elif [ "${POLICY}" == "manylinux2014" ]; then
-	if [ "${PLATFORM}" == "s390x" ]; then
-		BASEIMAGE="s390x/clefos:7"
-	else
-		BASEIMAGE="${MULTIARCH_PREFIX}centos:7"
-	fi
+if [ "${POLICY}" == "manylinux2014" ]; then
+        BASEIMAGE="${MULTIARCH_PREFIX}centos:7"
 	DEVTOOLSET_ROOTPATH="/opt/rh/devtoolset-9/root"
 	PREPEND_PATH="${DEVTOOLSET_ROOTPATH}/usr/bin:"
-	if [ "${PLATFORM}" == "i686" ]; then
-		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst"
-	else
-		LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib64:${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib64/dyninst:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst:/usr/local/lib64"
-	fi
-elif [ "${POLICY}" == "manylinux_2_24" ]; then
-	BASEIMAGE="${MULTIARCH_PREFIX}debian:9"
-	DEVTOOLSET_ROOTPATH=
-	PREPEND_PATH=
-	LD_LIBRARY_PATH_ARG=
+        LD_LIBRARY_PATH_ARG="${DEVTOOLSET_ROOTPATH}/usr/lib64:${DEVTOOLSET_ROOTPATH}/usr/lib:${DEVTOOLSET_ROOTPATH}/usr/lib64/dyninst:${DEVTOOLSET_ROOTPATH}/usr/lib/dyninst:/usr/local/lib64"
 else
 	echo "Unsupported policy: '${POLICY}'"
 	exit 1
